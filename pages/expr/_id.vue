bb7417fd5c9b4ed4395dce71c086ae5535ba0cd9
Geon George <geongeorge98@gmail.com>
2019-09-09 17:24:22 +0530

    added dynamic id posts in /expr/:id

---
+++ pages/expr/_id.vue
@@ -0,0 +1,45 @@
+<template>
+  <expr :iregex="new RegExp(myregex.regex)" :iflag="myregex.flag" :imatchText="myregex.matchText" :eHeight="myregex.embedHeight">
+    <span slot="title">{{myregex.title}}</span>
+
+    <span slot="tagline">{{myregex.tagline}}</span>
+
+    <p
+      slot="firstdescr"
+    >{{myregex.firstdescr}}</p>
+
+    <div slot="seconddescr">
+      <p>{{myregex.seconddescr}}</p>
+      <br />
+      <CheatTable :inputRegex="myregex.cheatRegex"></CheatTable>
+    </div>
+  </expr>
+</template>
+
+<script>
+import expr from "~/layout-g/expr.vue";
+import CheatTable from "~/components/post-components/CheatTable";
+import regexdata from "~/static/regexdata.json"
+export default {
+  components: {
+    expr,
+    CheatTable
+  },
+  data() {
+    return {
+        // regex: regexdata
+        id: this.$route.params.id
+    };
+  },
+  computed: {
+      myregex() {
+          return regexdata.find((val)=>{
+              return val.id == this.id
+          })
+      }
+  }
+};
+</script>
+
+<style>
+</style>
