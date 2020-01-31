d17a7a26dc39ba8a3cf06d2961525072b2e9598e
Geon George <geongeorge98@gmail.com>
2020-01-31 12:39:58 +0530

    added 404 page

---
+++ layouts/error.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container mx-auto md:pt-5 lg:pt-20 px-4 max-w-2xl">
+    <div class="text-center">
+        <logo></logo>
+      <h1 class="text-2xl" v-if="error.statusCode === 404">Page not found</h1>
+      <h1 v-else>An error occurred</h1>
+      <nuxt-link to="/" class="text-red-600 hover:underline">go back to home page</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/utils/Logo'
+export default {
+
+  props: ['error'],
+  components: {
+      Logo
+  }
+}
+</script>
