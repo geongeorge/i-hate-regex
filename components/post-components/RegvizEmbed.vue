2732917f2d8adf6a73c6160c51619276932e0797
Geon George <geongeorge98@gmail.com>
2020-02-02 18:11:57 +0530

    added copy embed code

--- components/post-components/RegvizEmbed.vue
+++ components/post-components/RegvizEmbed.vue
@@ -8,6 +8,11 @@
         :height="height"
         frameborder="0"
       />
+      <div class="hidden md:block mr-3">
+        <p class="text-right text-sm opacity-50 hover:opacity-100 hover:underline">
+          <a href="#" @click.prevent="copyEmbedCode()">&lt;/&gt; embed code</a>
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -21,7 +26,9 @@ export default {
     // embedRoot: {default: 'http://localhost:3300/make/'},
   },
   data() {
-    return {}
+    return {
+      embedUrl : ""
+    }
   },
   watch: {
     regex() {
@@ -34,9 +41,9 @@ export default {
   },
   methods: {
     reload() {
-      var embedUrl = this.embedRoot + this.hashEncodeUrl(this.regex.source)
+      this.embedUrl = this.embedRoot + this.hashEncodeUrl(this.regex.source)
       // console.log("source", this.regex.source, embedUrl)
-      this.$refs.regviz.src = embedUrl
+      this.$refs.regviz.src = this.embedUrl
     },
     // to encode base64 for url
     //https://gist.github.com/geongeorge/c30e9b1e3e7590b8a22464c879ad9a04
@@ -47,6 +54,40 @@ export default {
         .replace(/\//g, "_") //  /..
         .replace(/\\/g, ",") //  \
         .replace(/\=+$/, "") // eslint-disable-line
+    },
+
+
+    copyEmbedCode() {
+
+      var tempInput = document.createElement("input");
+      tempInput.style = "position: absolute; left: -1000px; top: -1000px";
+      tempInput.value = this.embedString;
+      document.body.appendChild(tempInput);
+      tempInput.select();
+
+      try {
+        document.execCommand("copy")
+        this.$toast.success("Copied")
+      } catch (err) {
+        this.$toast.error("Oops, unable to copy")
+      }
+
+      document.body.removeChild(tempInput);
+
+      /* unselect the range */
+      // regexToCopy.setAttribute('type', 'hidden')
+      window.getSelection().removeAllRanges()
+    }
+  },
+  computed: {
+    embedString() {
+      return `<iframe
+        src="${this.embedUrl}"
+        height="${this.height}"
+        style="width:100%"
+        frameborder="0"
+      />
+      `
     }
   }
 }
