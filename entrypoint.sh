5b7f57831e9901014f4178e8624d5fea6964b95a
EbolaWare <averittexpress@gmail.com>
2020-02-10 17:39:26 -0600

    clean up unnecessary comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
-#apk add yarn git
-#git clone https://github.com/geongeorge/i-hate-regex.git
 cd /i-hate-regex/
-#sed -i 's/localhost/0.0.0.0/g' nuxt.config.js
-#yarn install
-#yarn build
 yarn start
