8e46d2ef09d5f5e77db4e456a58e4cad50ecd229
Geon George <geongeorge98@gmail.com>
2020-04-23 17:52:26 +0530

    fix: tried to fix eslint and prettier 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,25 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:prettier/recommended',
-    'plugin:vue/recommended',
-    'prettier'
-  ],
-  // required to lint *.vue files
-  plugins: ['vue'],
-  // add your custom rules here
-  rules: {
-    'prettier/prettier': 'error',
-    'no-console': 'off',
-    'vue/max-attributes-per-line': 'off',
-    'vue/html-self-closing': 0
-  }
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+    },
+    parserOptions: {
+        parser: 'babel-eslint',
+    },
+    extends: [
+        // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+        // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+        'plugin:vue/recommended',
+        'prettier',
+    ],
+    // required to lint *.vue files
+    plugins: ['vue'],
+    // add your custom rules here
+    rules: {
+        // 'prettier/prettier': 'error',
+        'no-console': 'off',
+        'vue/max-attributes-per-line': 'off',
+        'vue/html-self-closing': 0,
+    },
 }
