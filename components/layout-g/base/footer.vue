bde619f019c6b769ab77f61da4117383c653bde6
Geon George <geongeorge98@gmail.com>
2020-06-16 13:00:45 +0530

    Sponsors: Moved upwards and added carbon ads

--- components/layout-g/base/footer.vue
+++ components/layout-g/base/footer.vue
@@ -8,28 +8,8 @@
           </div>
           <div class="px-2 text-gray-500">
             by
-            <a href="https://geongeorge.com/" class="underline" target="_blank"
-              >geon</a
-            >
+            <a href="https://geongeorge.com/" class="underline" target="_blank">geon</a>
           </div>
-        </div>
-      </div>
-      <div class="flex-initial">
-        <div class="flex flex-col h-full w-full justify-center mr-4">
-          <h2 class="text-gray-500">
-            Sponsors:
-          </h2>
-          <a
-            href="http://bit.ly/datree-ihr"
-            rel="noopener nofollow"
-            target="_blank"
-          >
-            <img
-              class="sponsor"
-              src="https://assets.website-files.com/5d514f718e8309c01d798013/5d8b4657e7740e300666e3cc_datree_LOGO%20FLAT%20h-p-500.png"
-              alt="datree.io"
-            />
-          </a>
         </div>
       </div>
     </div>
@@ -41,14 +21,5 @@ export default {}
 </script>
 
 <style>
-.sponsor {
-  width: 120px;
-  filter: grayscale(100%);
-  opacity: 0.7;
-  transition: filter opacity 200ms ease-out;
-}
-.sponsor:hover {
-  filter: grayscale(10%);
-  opacity: 1;
-}
+
 </style>
--- components/layout-g/expr.vue
+++ components/layout-g/expr.vue
@@ -21,7 +21,34 @@
         <RegvizEmbed :regex="regex" :height="eHeight" />
       </div>
       <div class="mt-8">
-        <slot name="seconddescr" />
+        <div class="md:flex">
+          <div>
+            <slot name="seconddescr" />
+          </div>
+          <div class="md:w-1/4 px-6 min-w-64 flex-col justify-center">
+            <div class="font-bold text-gray-500 text-center">
+              Our Sponsors
+            </div>
+
+            <div class="mx-auto w-48 mt-6 sponsor-effect">
+              <a
+                href="http://bit.ly/datree-ihr"
+                rel="noopener nofollow"
+                target="_blank"
+              >
+                <img
+                  class="sponsor mx-auto"
+                  src="https://assets.website-files.com/5d514f718e8309c01d798013/5d8b4657e7740e300666e3cc_datree_LOGO%20FLAT%20h-p-500.png"
+                  alt="datree.io"
+                />
+              </a>
+            </div>
+            <div class="mx-auto mt-6 sponsor-effect">
+              <!-- carbon ads -->
+              <script id="_carbonads_js" async type="text/javascript" src="//cdn.carbonads.com/carbon.js?serve=CE7DL23E&placement=ihateregexio"></script>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </post>
@@ -71,3 +98,80 @@ export default {
   }
 }
 </script>
+<style>
+.sponsor-effect {
+  opacity: 0.7;
+  transition: opacity 200ms ease-out;
+}
+.sponsor-effect:hover {
+  opacity: 1;
+}
+.sponsor {
+  width: 130px;
+}
+#carbonads {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu,
+  Cantarell, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+#carbonads {
+  display: flex;
+  max-width: 170px;
+  margin: 0 auto;
+  text-align: center;
+  /* background-color: hsl(0, 0%, 98%); */
+  /* box-shadow: 0 1px 4px 1px hsla(0, 0%, 0%, .1); */
+}
+
+#carbonads a {
+  color: inherit;
+  text-decoration: none;
+}
+
+#carbonads a:hover {
+  color: inherit;
+}
+
+#carbonads span {
+  position: relative;
+  display: block;
+  overflow: hidden;
+}
+
+#carbonads .carbon-wrap {
+  /* display: flex; */
+  width: 100%;
+}
+
+.carbon-img {
+  display: block;
+  margin: 0;
+  line-height: 1;
+}
+
+.carbon-img img {
+  display: block;
+  margin: 0 auto;
+  width:100%;
+}
+
+.carbon-text {
+  font-size: 13px;
+  padding: 10px;
+  text-align: center;
+  line-height: 1.5;
+  text-align: left;
+}
+
+.carbon-poweredby {
+  display: block;
+  padding: 8px 10px;
+  /* background: repeating-linear-gradient(-45deg, transparent, transparent 5px, hsla(0, 0%, 0%, .025) 5px, hsla(0, 0%, 0%, .025) 10px) hsla(203, 11%, 95%, .4); */
+  text-align: center;
+  text-transform: uppercase;
+  letter-spacing: .5px;
+  font-weight: 600;
+  font-size: 9px;
+  line-height: 1;
+}
+</style>
