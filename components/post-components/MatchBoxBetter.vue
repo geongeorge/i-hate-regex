5405fe21e431d15eade15cf8aaf77aba023875ac
Geon George <geongeorge98@gmail.com>
2020-08-10 15:27:45 +0530

    Changes var to let

--- components/post-components/MatchBoxBetter.vue
+++ components/post-components/MatchBoxBetter.vue
@@ -36,10 +36,12 @@
 </template>
 
 <script>
-import TransitionExpand from './TransitionExpand'
-import pastePlainText from '~/mixins/pastePlainText.js'
-import autosize from 'autosize'
-var typeTimeout
+import TransitionExpand from "./TransitionExpand";
+import pastePlainText from "~/mixins/pastePlainText.js";
+import autosize from "autosize";
+
+// Debounce timer for editing change
+let typeTimeout;
 
 export default {
   components: {
@@ -48,7 +50,7 @@ export default {
   mixins: [pastePlainText],
   props: {
     regex: { required: true },
-    sampleText: { default: ['Lorem ipsum'] }
+    sampleText: { default: ["Lorem ipsum"] }
   },
   data() {
     return {
@@ -56,83 +58,83 @@ export default {
       queries: [],
       isTyping: false,
       loaded: false
-    }
+    };
   },
   computed: {
     showText() {
       if (this.displayMatches) {
-        return 'hide matches'
+        return "hide matches";
       }
-      return 'show matches'
+      return "show matches";
     },
     dataText() {
-      return this.sampleText.join('\n') || ''
+      return this.sampleText.join("\n") || "";
     }
   },
   watch: {
     regex: function() {
       // eslint-disable-line
-      this.boxEdited()
+      this.boxEdited();
     }
   },
   mounted() {
-    this.boxEdited()
+    this.boxEdited();
     // setTimeout(this.boxEdited(), 50)
 
     //set loaded to true
-    this.loaded = true
+    this.loaded = true;
   },
   methods: {
     toggleMatches() {
-      this.displayMatches = !this.displayMatches
-      this.boxEdited()
+      this.displayMatches = !this.displayMatches;
+      this.boxEdited();
     },
     htmlEncode(s) {
       return s
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')
-        .replace(/"/g, '&quot;')
-        .replace(/'/g, '&#039;')
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
     },
     resizeTextarea() {
       // fix rreduction of height
-      let textarea = this.$refs.matchbox
-      autosize(textarea)
+      let textarea = this.$refs.matchbox;
+      autosize(textarea);
     },
     getMatchTextOnly() {
-      const element = this.$refs.matchbox
+      const element = this.$refs.matchbox;
       const text =
-        element.value || element.innerText || element.textContent || '' //get inner text without html
-      return text
+        element.value || element.innerText || element.textContent || ""; //get inner text without html
+      return text;
     },
     applyHighlights(text) {
-      let fixedText = this.htmlEncode(text)
-      fixedText = fixedText.replace(/\n$/g, '\n\n')
-      fixedText = `<mark>${fixedText}</mark>`
-      return fixedText
+      let fixedText = this.htmlEncode(text);
+      fixedText = fixedText.replace(/\n$/g, "\n\n");
+      fixedText = `<mark>${fixedText}</mark>`;
+      return fixedText;
     },
     hideCanvas(val = true) {
-      const highlights = this.$refs.highlights
+      const highlights = this.$refs.highlights;
 
       if (val) {
-        highlights.classList.add('hidden')
+        highlights.classList.add("hidden");
       } else {
-        highlights.classList.remove('hidden')
+        highlights.classList.remove("hidden");
       }
     },
     boxEdited() {
       if (process.client) {
-        let text = this.getMatchTextOnly()
-        let markedText = ''
-        let matches
+        let text = this.getMatchTextOnly();
+        let markedText = "";
+        let matches;
         // Check for g flag, if not don't use matchAll
         // see https://stackoverflow.com/a/60290199/8134823
         // matchAll throws an error when g flag is missing
-        if (this.regex.flags.includes('g')) {
-          matches = [...text.matchAll(this.regex)]
+        if (this.regex.flags.includes("g")) {
+          matches = [...text.matchAll(this.regex)];
         } else {
           // if matches? add to array else empty []
-          matches = text.match(this.regex) ? [text.match(this.regex)] : []
+          matches = text.match(this.regex) ? [text.match(this.regex)] : [];
         }
 
         // each match will have
@@ -141,8 +143,8 @@ export default {
         //   index -> pos of
         //   input -> input str
         //   length -> number of matches incl groups
-        let lastEndPos = 0
-        let lastPart = ''
+        let lastEndPos = 0;
+        let lastPart = "";
         for (let match of matches) {
           // The text is divided into 4 parts
           //   alreadyParsedText => which need not be encoded(fixed)
@@ -152,51 +154,51 @@ export default {
           //               Encoded, but added <mark></mark> tags which will not be encoded for highlights
           //   lastPart => rest of the string, not highlighted/fixed
           //
-          const startPos = match.index
-          const endPos = startPos + match[0].length
+          const startPos = match.index;
+          const endPos = startPos + match[0].length;
 
-          const lengthOfBeginPart = startPos - lastEndPos
+          const lengthOfBeginPart = startPos - lastEndPos;
 
-          const beginPart = text.substr(lastEndPos, lengthOfBeginPart)
-          lastPart = text.substr(endPos)
+          const beginPart = text.substr(lastEndPos, lengthOfBeginPart);
+          lastPart = text.substr(endPos);
 
           //string matched
-          const matchStr = text.substr(startPos, match[0].length)
+          const matchStr = text.substr(startPos, match[0].length);
 
-          const encodedMatchStr = this.applyHighlights(matchStr)
+          const encodedMatchStr = this.applyHighlights(matchStr);
           // html encoded parts
-          const encodedBeginPart = this.htmlEncode(beginPart)
+          const encodedBeginPart = this.htmlEncode(beginPart);
 
           // text from begining to match string (including)
-          const textTillStr = encodedBeginPart + encodedMatchStr
+          const textTillStr = encodedBeginPart + encodedMatchStr;
 
-          markedText += textTillStr
+          markedText += textTillStr;
 
           // Add lastIndex the length of begin part + matchStr
-          lastEndPos = endPos
+          lastEndPos = endPos;
         }
         // add the final unmatched lastPart
-        markedText += this.htmlEncode(lastPart)
+        markedText += this.htmlEncode(lastPart);
 
-        this.$refs.highlights.innerHTML = markedText
+        this.$refs.highlights.innerHTML = markedText;
 
         setTimeout(() => {
-          this.resizeTextarea()
-        }, 50)
+          this.resizeTextarea();
+        }, 50);
       }
     },
     matchboxChanged() {
       // eslint-disable-line
-      this.resizeTextarea()
+      this.resizeTextarea();
       if (typeTimeout) {
-        clearTimeout(typeTimeout)
+        clearTimeout(typeTimeout);
       }
       typeTimeout = setTimeout(() => {
-        this.boxEdited()
-      }, 50)
+        this.boxEdited();
+      }, 50);
     }
   }
-}
+};
 </script>
 
 <style>
