a30de101cfec05385d5274996e28905e28d2aabc
Geon George <geongeorge98@gmail.com>
2020-08-10 14:02:06 +0530

    Adds .dark-mode

--- assets/styles/tailwind.config.js
+++ assets/styles/tailwind.config.js
@@ -1,14 +1,25 @@
 module.exports = {
   theme: {
     fontFamily: {
+      // compatible with @nuxtjs/color-mode
+      darkSelector: ".dark-mode",
       sans: ["'Source Sans'", "-apple-system", "BlinkMacSystemFont"]
     }
   },
   variants: {
-    textColor: ["group-hover", "hover"],
+    backgroundColor: [
+      "dark",
+      "dark-hover",
+      "dark-group-hover",
+      "dark-even",
+      "dark-odd"
+    ],
+    textColor: ["group-hover", "hover", "dark", "dark-hover", "dark-active"],
+    borderColor: ["dark", "dark-focus", "dark-focus-within"],
     opacity: ["group-hover", "hover"],
     border: ["hover"],
     width: ["responsive"]
   },
-  purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"]
+  purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
+  plugins: [require("tailwindcss-dark-mode")()]
 };
