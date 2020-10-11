ec97f8f0e4d261e6803807429952f5897b1a63a6
Geon George <geongeorge98@gmail.com>
2020-10-11 10:24:12 +0530

    Adds donate page

---
+++ pages/donate.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="expr-header">
+      <h1>Donate</h1>
+      <h3>Support the development of this project</h3>
+    </div>
+    <p>
+      If you find this useful you can support buy buying me a coffee :)
+    </p>
+  </div>
+</template>
+
+<script>
+import expr from "~/components/layouts/expr.vue";
+export default {
+  layout: 'article'
+};
+</script>
+
+<style></style>
